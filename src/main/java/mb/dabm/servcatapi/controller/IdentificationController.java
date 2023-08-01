266dd78a0559382b97c84c69eacf7f31521e52cc
Carlos Anders <carlosanders@gmail.com>
2023-08-01 01:19:20 -0300

    [ANDERS] logs

--- src/main/java/mb/dabm/servcatapi/controller/IdentificationController.java
+++ src/main/java/mb/dabm/servcatapi/controller/IdentificationController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import mb.dabm.servcatapi.ServcatApiApplication;
 import mb.dabm.servcatapi.entity.Identification;
 import mb.dabm.servcatapi.model.IdentificationDto;
 import mb.dabm.servcatapi.service.IdentificationService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,8 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class IdentificationController {
 
+//    Logger logger = LogManager.getLogger(IdentificationController.class);
+
     @Autowired
     IdentificationService service;
 
@@ -28,6 +33,13 @@ public class IdentificationController {
         @RequestParam(value = "page", defaultValue = "0") int page,
         @RequestParam(value = "size", defaultValue = "20") int size
     ) {
+//        logger.trace("TRACE");
+//        logger.debug("DEBUG");
+//        logger.info("INFO");
+//        logger.warn("WARN");
+//        logger.error("ERROR");
+//        logger.fatal("FATAL");
+
         return ResponseEntity.ok(service.findAll(page, size));
     }
 
